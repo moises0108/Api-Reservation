1a132a1aabe4f594c6824c6d09a53a7c69f6719e
Moises Patino <moisespatinoph@gmail.com>
2022-08-27 23:51:59 -0400

    fixing models

--- app_reservation/models.py
+++ app_reservation/models.py
@@ -8,19 +8,6 @@ from sqlalchemy.schema import Table
 #app_reservation
 from .database import Base
 
-service_workers_table = Table(
-    "association",
-    Base.metadata,
-    Column("services_id", ForeignKey("services.id"),primary_key=True),
-    Column("workers_id", ForeignKey("workers.id"),primary_key=True),
-)
-
-workers_disponibility_table = Table(
-    "association",
-    Base.metadata,
-    Column("disponibility_id", ForeignKey("disponibility.id"),primary_key=True),
-    Column("workers_id", ForeignKey("workers.id"),primary_key=True),
-)
 
 
 class User(Base):
@@ -35,7 +22,18 @@ class User(Base):
     is_active=Column(Boolean,default=True)
     created_at=Column(DateTime,default=datetime.now())
 
-    service = relationship("Service", back_populates="user")
+    user_service = relationship("UserService", back_populates="users_services")
+
+class UserService(Base):
+    __tablename__="users_services"
+    id = Column(Integer,primary_key=True,index=True)
+
+    users_id=Column(Integer,ForeignKey("users.id"))
+    users = relationship("User", back_populates="users_services")
+
+    services_id=Column(Integer,ForeignKey("services.id"))
+    services = relationship("Service", back_populates="users_services")
+
 
 class Service(Base):
     __tablename__="services"
@@ -43,9 +41,20 @@ class Service(Base):
     id = Column(Integer,primary_key=True,index=True)
     name_service = Column(String)
 
-    users_id=Column(Integer,ForeignKey("users.id"))
-    users = relationship("User", back_populates="services")
-    workers= relationship("workers",secondary=service_workers_table)
+    user_service = relationship("UserService", back_populates="users_services")
+    service_worker = relationship("ServiceWorker", back_populates="services_workers")
+
+
+class ServiceWorker(Base):
+    __tablename__="services_workers"
+    id = Column(Integer,primary_key=True,index=True)
+
+    services_id=Column(Integer,ForeignKey("users.id"))
+    services = relationship("Service", back_populates="services_workers")
+
+    services_id=Column(Integer,ForeignKey("workers.id"))
+    workers = relationship("Worker", back_populates="services_workers")
+
 
 class Worker(Base):
     __tablename__="workers"
@@ -55,13 +64,25 @@ class Worker(Base):
     last_name = Column(String)
 
     
-    disponibility= relationship("Disponibility",back_populates="Worker")
+    service_worker = relationship("ServiceWorker", back_populates="services_workers")
+    worker_disponibility = relationship("WorkerDisponibility", back_populates="workers_disponibility")
+
+
+class WorkerDisponibility(Base):
+    __tablename__="workers_disponibility"
+    id = Column(Integer,primary_key=True,index=True)
+
+    services_id=Column(Integer,ForeignKey("workers.id"))
+    services = relationship("Worker", back_populates="workers_disponibility")
+
+    services_id=Column(Integer,ForeignKey("disponibility.id"))
+    disponibility = relationship("Disponibility", back_populates="workers_disponibility")
 
 class Disponibility(Base):
     __tablename__="disponibility"
 
     id = Column(Integer,primary_key=True,index=True)
-    date_disponible = Column(DateTime)
-    hour_disponible = Column(Integer)
+    date_disponible = Column(DateTime,unique=True)
+    hour_disponible = Column(Integer,unique=True)
 
-    workers=relationship("Worker",back_populates="disponibility")
+    worker_disponibility = relationship("WorkerDisponibility", back_populates="workers_disponibility")

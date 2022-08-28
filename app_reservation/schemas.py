a63a9092368345d2a0f77585399814d8a6fa24e0
Moises Patino <moisespatinoph@gmail.com>
2022-08-28 00:11:01 -0400

    schemas disponibility

--- app_reservation/schemas.py
+++ app_reservation/schemas.py
@@ -1,6 +1,7 @@
 #Python
 from datetime import date
 from typing import Optional
+from xmlrpc.client import DateTime
 #Pydantic
 from pydantic import BaseModel
 from pydantic import Field,EmailStr
@@ -21,6 +22,7 @@ class Person(BaseModel):
         min_length=1,
         max_length=100)
 
+
 class User(Person):
     email:EmailStr=Field(
         ...,
@@ -35,6 +37,7 @@ class User(Person):
         min_length=1,
         max_length=100)
 
+
 class Service(BaseModel):
     service_name:str=Field(
         ...,
@@ -45,3 +48,17 @@ class Service(BaseModel):
 
 class Worker(Person):
     pass
+
+
+class Disponibility(BaseModel):
+    date_disponible:date=Field(
+        ...,
+        title="date disponible",
+        example="2023-08-01"
+        )
+    hour_disponible:int=Field(
+        ...,
+        lt=10,
+        title="hour disponible",
+        description="hour that you want to agend",
+        )

61b054d45a4b597eaf724667fd7e31eb649a36e4
Moises Patino <moisespatinoph@gmail.com>
2022-08-30 02:12:50 -0400

    get user by email crud

--- app_reservation/crud.py
+++ app_reservation/crud.py
@@ -10,3 +10,6 @@ def create_user(db:Session,user:schemas.UserRegister):
     db.commit
     db.refresh
     return {'msg':'Created Succesfully'}
+
+def get_user_by_email(db:Session,email:str):
+    return db.query(models.User).filter(models.User.email==email).first()
